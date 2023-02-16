3b6e52e2d7608596950ccca4d888058c10655e5e
Saad Siddiqui <92918978+supremed3v@users.noreply.github.com>
2023-02-16 14:42:14 +0000

    recursive binary search

---
+++ recursiveBinarySearch.js
@@ -0,0 +1,7 @@
+function recursiveBinarySearch(arr, target){
+  
+}
+
+console.log(recursiveBinarySearch([-5, 3, 4, 6, 10], 10))
+console.log(recursiveBinarySearch([-5, 3, 4, 6, 10], 6))
+console.log(recursiveBinarySearch([-5, 3, 4, 6, 10], 20))
