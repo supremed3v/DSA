e208663d37509dfb295a9b45b286b3091a4d08a5
Saad Siddiqui <92918978+supremed3v@users.noreply.github.com>
2023-02-16 14:18:27 +0000

    Binary Search

---
+++ binarySearch.js
@@ -0,0 +1,7 @@
+function binarySearch(arr, target){
+  let startIndex = 0
+}
+
+console.log(binarySearch(([-5, 3, 5, 10, 20], 3)))
+console.log(binarySearch(([-5, 3, 5, 10, 20], 20)))
+console.log(binarySearch(([-5, 3, 5, 10, 20], 21)))
