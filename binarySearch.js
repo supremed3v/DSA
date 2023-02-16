89b03b02118223dcab21384ab8b5d60677fb4db3
Saad Siddiqui <92918978+supremed3v@users.noreply.github.com>
2023-02-16 14:36:27 +0000

    completed binary search algo

--- binarySearch.js
+++ binarySearch.js
@@ -1,7 +1,22 @@
 function binarySearch(arr, target){
-  let startIndex = 0
+  // Pointers
+  let startIndex = 0;
+  let endIndex = arr.length - 1
+  
+  while(startIndex <= endIndex){
+    let middleIndex = Math.floor((startIndex + endIndex) / 2)
+    if(target === arr[middleIndex]){
+      return middleIndex
+    }
+    if(target < arr[middleIndex]){
+      endIndex = middleIndex - 1
+    } else {
+      startIndex = middleIndex + 1
+    }
+  }
+  return -1 
 }
 
-console.log(binarySearch(([-5, 3, 5, 10, 20], 3)))
-console.log(binarySearch(([-5, 3, 5, 10, 20], 20)))
-console.log(binarySearch(([-5, 3, 5, 10, 20], 21)))
+console.log(binarySearch([-5, 3, 5, 10, 20], 3)) // 3
+console.log(binarySearch([-5, 3, 5, 10, 20], 20)) // 
+console.log(binarySearch([-5, 3, 5, 10, 20], 21))
